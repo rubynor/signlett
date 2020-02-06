b3e58ab31b944dffb53cf69de871c2e5d377a231
Ove Simon Wernersson <simon.wernersson@live.no>
2020-02-06 10:47:53 +0100

    Add document model and controller

---
+++ db/migrate/20200206093953_create_documents.rb
@@ -0,0 +1,11 @@
+class CreateDocuments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :documents do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :status
+      t.string :file_path
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/fabricators/document_fabricator.rb
@@ -0,0 +1,5 @@
+Fabricator(:document) do
+  user      nil
+  status    1
+  file_path "MyString"
+end
