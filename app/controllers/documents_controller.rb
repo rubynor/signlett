0e9c75a93e30d10602ba301c50495f16046292cd
Ove Simon Wernersson <simon.wernersson@live.no>
2020-02-06 16:03:52 +0100

    Add method for only showing documents connected to user

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,8 +1,10 @@
 class DocumentsController < ApplicationController
   def index
+    @documents = Document.find_by_user_id(current_user)
   end
 
   def show
+    @document = Document.find(params[:id])
   end
 
   def create
--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,11 @@
 module DocumentsHelper
+  def document_status(status)
+    status_array = ['Ikke Signert', 'Delvis signert', 'Signert']
+
+    status_array[status]
+  end
+
+  def full_name
+    current_user.first_name.concat(" #{current_user.last_name}")
+  end
 end
