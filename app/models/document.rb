f10b7f1b18b36d5a54be098dd6d4a6f9d307c6e7
simwer <simon.wernersson@live.no>
2020-03-16 18:39:39 +0100

    Fixed issue for uploading

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,6 @@ class Document < ApplicationRecord
   belongs_to :user
   has_many :recipient, dependent: :destroy
 
-  has_many_attached :file, dependent: :purge_later
+  has_one_attached :file, dependent: :purge_later
 
 end
