3156f55c36b02764cd286b0f263bd725af8f77a6
simwer <simon.wernersson@live.no>
2020-03-17 14:57:11 +0100

    New document display && capitalize fcuntion

--- app/javascript/components/document-list.vue
+++ app/javascript/components/document-list.vue
@@ -1,12 +1,105 @@
 <template>
     <span>
-        <v-data-table
-        ></v-data-table>
+        <v-row>
+            <v-col
+                cols="10"
+                offset-sm="1"
+            >
+                <span
+                    class="display-1 font-weight-light"
+                >
+                    Mine dokumenter
+                </span>
+            </v-col>
+        </v-row>
+        <v-data-iterator
+            :items="documentForUser"
+        >
+            <template v-slot:default="props">
+                <v-row>
+                    <v-col
+                        cols="12"
+                        sm="10"
+                        offset-sm="1"
+                        class=" elevation-1 pa-0 mt-5"
+
+                    >
+                        <v-row class="ma-0 grey--text text--lighten-1 pa-0">
+                            <v-col
+                                    cols="3"
+                            >
+                                Dokument
+                            </v-col>
+                            <v-col
+                                    cols="3"
+                            >
+                                Størrelse
+                            </v-col>
+                            <v-col
+                                    cols="3"
+                            >
+                                Status
+                            </v-col>
+                            <v-col
+                                    cols="3"
+                            >
+                                Handlinger
+                            </v-col>
+                        </v-row>
+                        <span
+                          v-for="document in props.items"
+                            :key="document.id"
+                        >
+                            <v-hover v-slot:default="{ hover }">
+                                <div
+                                    :class="{ 'on-hover': hover }"
+                                    class="font-weight-light"
+                                >
+                                    <v-row class="ma-0 pa-0">
+                                        <v-col
+                                                cols="3"
+                                        >
+                                            {{capitalize(document.file.filename)}}
+                                        </v-col>
+                                        <v-col
+                                                cols="3"
+                                        >
+                                            {{document.file.byteSize/1000}} kb
+                                        </v-col>
+                                        <v-col
+                                                cols="3"
+                                        >
+                                            {{determineStatus(document.status)}}
+
+                                        </v-col>
+                                        <v-col
+                                                cols="3"
+                                        >
+                                            {{document.file.filename}}
+                                        </v-col>
+                                    </v-row>
+                                </div>
+                            </v-hover>
+                        </span>
+                    </v-col>
+                </v-row>
+            </template>
+        </v-data-iterator>
     </span>
 </template>
 
+<style>
+    .on-hover{
+        border: solid 1px rgba(187,222,251,0.8) !important;
+        -moz-box-shadow: 0 0 5px #ccc;
+        -webkit-box-shadow: 0 0 5px #ccc;
+        box-shadow: 0 0 5px #ccc;
+    }
+</style>
+
 <script>
     import {DOCUMENT_FOR_USER} from "../constants/graphql";
+    import { capitalize } from "../network/vue-rails";
 
     export default {
         name: 'DocumentList',
@@ -24,23 +117,21 @@
             }
         },
         methods: {
-          determineStatus(status){
+            capitalize,
+            determineStatus(status){
               switch (status) {
-                case 0: "mdi-alert";
+                case 0: return "Venter på signering";
                     break;
                 case 1:
-                    "mdi-progress-check"
-                    console.log(status)
-
+                    return "Delvis signert"
                     break;
                 case 2:
-                    "mdi-check-circle-outline"
-                    console.log(status)
+                    return "Signert"
                     break;
                 default:
                     break;
               }
-          }
+            }
         },
         apollo: {
             documentForUser: {
