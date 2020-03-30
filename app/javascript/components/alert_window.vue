02f20869b5f0e7d91f6478e446c8cc4e2b3b3150
simwer <simon.wernersson@live.no>
2020-03-30 10:45:15 +0200

    Sentralized alert component v1.0

---
+++ app/javascript/components/alert_window.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-alert
+            :value="alertContent.show"
+            text
+            :type="alertContent.type"
+            border="left"
+            transition="scale-transition"
+    >
+        {{ alertContent.message }}
+    </v-alert>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                alertWindow: false,
+                alertType: ''
+            }
+        },
+        props: ['alertContent']
+    }
+</script>
