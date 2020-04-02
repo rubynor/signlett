ffb0f094c3a005f9dec26e4ac488e4af57ae30ed
simwer <simon.wernersson@live.no>
2020-04-02 11:08:48 +0200

    Add ability to switch between components in documentview

--- app/javascript/components/document_view.vue
+++ app/javascript/components/document_view.vue
@@ -1,23 +1,40 @@
 <template>
     <span>
-        <DocumentList/>
-        <FileUpload/>
+        <v-row>
+            <!-- "Buttons" for changing view from document-list to recipients -->
+            <v-col
+                    lg="10"
+                    sm="12"
+                    offset-lg="1"
+                    class="pl-0"
+            >
+                <a @click="active = !active" :class="active ? 'secondary--text' : 'black--text'" class="font-weight-medium">Mine Signeringer</a>
+                <span class="align-self-md-center secondary--text mx-4">/</span>
+                <a @click="active = !active" :class="active ? 'black--text' : 'secondary--text'" class="font-weight-medium ">Signeringer til meg</a>
+            </v-col>
+        </v-row>
+        <!-- Components show dependant on what is active -->
+        <DocumentList v-show="active"/>
     </span>
 </template>
 
+<style>
+</style>
+
 <script>
     import DocumentList from './document-list'
-    import FileUpload from './file_upload_dialog'
 
     export default {
         data(){
             return {
-
+                active: true
             }
         },
         components: {
             DocumentList,
-            FileUpload
+        },
+        methods: {
+
         }
     }
 </script>
