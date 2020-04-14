0df5c304e40403d2f332a8bcbc4d79f3f2662768
Pål Andre Sundt <pal@rubynor.com>
2020-04-14 16:16:33 +0200

    Update sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,9 +117,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'heroku.com',
+      :domain => 'signlett.no',
       :address => 'smtp.sendgrid.net',
-      :port => '587',
+      :port => 587,
       :authentication => :plain,
       :enable_starttls_auto => true
   }
