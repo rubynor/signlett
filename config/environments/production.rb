9e96bc3e7847369bf96ec67688f1d2302ae7f752
simwer <simon.wernersson@live.no>
2020-04-14 14:23:28 +0200

    Change env for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,8 +115,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'https://signlett.herokuapp.com/'}
   config.action_mailer.smtp_settings = {
-      :user_name => ENV['SENDGRID_PASSWORD'],
-      :password => ENV['SENDGRID_USERNAME'],
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
       :domain => 'https://signlett.herokuapp.com/',
       :address => 'smtp.sendgrid.net',
       :port => 465,
