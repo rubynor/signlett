4d11d9690d5e5687419e3fdf958affcd99634a70
simwer <simon.wernersson@live.no>
2020-04-15 13:44:26 +0200

    change host for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,7 +113,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://signlett.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'https://signlett.no/'}
   config.action_mailer.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
