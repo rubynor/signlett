3d9e347418db3af4ba56ebbee6933b7f0c94fb25
simwer <simon.wernersson@live.no>
2020-04-16 13:00:21 +0200

    Change regex

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,5 +1,5 @@
 class ApplicationMailbox < ActionMailbox::Base
   # routing /something/i => :somewhere
-  routing /(.+)@signlett.no/i => :bouncer
   routing /signature-(.+)@signlett.no/i => :sign
+  routing :all => :bouncer
 end
