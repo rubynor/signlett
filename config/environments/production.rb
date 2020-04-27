145a6a368f7e564ea72a3c0b1a783bfbdaf6cb8e
simwer <simon.wernersson@live.no>
2020-04-27 13:49:24 +0200

    change host for images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'http://www.signlett.no'
-  config.action_mailer.asset_host = config.action_controller.asset_host
+  config.action_mailer.asset_host = 'http://www.signlett.no'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
