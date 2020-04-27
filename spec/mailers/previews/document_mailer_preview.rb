e100b75b7eab2ff3279642d83f49cca017da9861
simwer <simon.wernersson@live.no>
2020-04-27 13:11:56 +0200

    New email design

--- spec/mailers/previews/document_mailer_preview.rb
+++ spec/mailers/previews/document_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/document_mailer
 class DocumentMailerPreview < ActionMailer::Preview
-
+  def signature_email
+    DocumentMailer.with(user: User.first, email: "someone@example.com", document: Document.first).signature_email
+  end
 end
